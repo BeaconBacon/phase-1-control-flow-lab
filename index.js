1b92f04e14a2ceba2723e9f97b95fd7b9baf7501
Christopher <bopherchan@gmail.com>
2023-01-31 01:06:43 -0500

    completed control flows lab

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(number){
+  let quote
+  if (number <= 400) {
+    quote = 'This one is on me!';
+  }
+  else if (number <= 2000) {
+    quote = 'That will be twenty bucks.'
+  }
+  else if (number < 2500) {
+    quote = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    quote = 'No can do.'
+  }
+  return quote
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  if (city == "NYC") {
+    return "Ok, sounds good."
+  }
+  else {
+    return "No go."
+  }
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(gratuity){
+  if (gratuity == "generous") {
+    return "Thank you so much."
+  }
+  else if (gratuity == "not as generous") {
+    return "Thank you."
+  }
+  else {
+    return "Bye."
+  }
+
   // Write your code here!
 }
